93974acbe7a8037819d24aea3e220ed52047d7ab
LeandroMirante <64902633+LeandroMirante@users.noreply.github.com>
2022-10-21 09:39:36 -0300

    Add files via upload

---
+++ system/users/admin.py
@@ -0,0 +1,74 @@
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.models import User
+from django.db.models import Q
+from .models import Customer, Company
+
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from .models import User
+
+
+class UserAdmin(BaseUserAdmin):
+    model = User
+    list_display = ('email', 'is_staff', 'is_active','city','district','address')
+    list_filter = ('email', 'is_staff', 'is_active','city','district','address')
+    fieldsets = (
+        (None, {'fields': ('email', 'name', 'password','city','district','address' )}),
+        ('Permissions', {'fields': ('is_staff', 'is_active', 'is_superuser', 'groups', 'user_permissions')}),
+    )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'name', 'password1', 'password2', 'is_active','city','district','address')}
+        ),
+    )
+    search_fields = ('email',)
+    ordering = ('email',)
+
+class CompanyAdmin(BaseUserAdmin):
+    model = User
+    list_display = ('email',  'is_active','city','district','address')
+    list_filter = ('email',  'is_active','city','district','address')
+    fieldsets = (
+        (None, {'fields': ('name','address','city','district','email','cnpj', 'is_active' )}),
+    )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('name','address','city','district','email','cnpj', 'password1', 'password2', 'is_active')}
+        ),
+    )
+    search_fields = ('email',)
+    ordering = ('email',)
+    def get_queryset(self, request):
+        qs = super().get_queryset(request)
+        if request.user.is_superuser:
+            return qs   
+        return qs.filter(email = request.user)
+
+class CustomerAdmin(BaseUserAdmin):
+    model = User
+    list_display = ('email',  'is_active','city','district','address','company_name')
+    list_filter = ('email',  'is_active','city','district','address','company_name')
+    fieldsets = (
+        (None, {'fields': ('name','cpf','email',  'is_active','company_name','documents' )}),
+    )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('name','cpf','email',  'password1', 'password2',  'is_active','company_name','documents' )}
+        ),
+    )
+    search_fields = ('email',)
+    ordering = ('email',)
+    def get_queryset(self, request):
+        qs = super().get_queryset(request)
+        if request.user.is_superuser:
+            return qs   
+        return qs.filter(Q(company_name= request.user) | Q(id = request.user.id))
+
+admin.site.register(User, UserAdmin)
+admin.site.register(Customer, CustomerAdmin)
+admin.site.register(Company, CompanyAdmin)
+
